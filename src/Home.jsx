e1f03baedffef33e9ffd2dd5f90bdb4d450c0311
LZYLRRY <162039530+LZYLRRY@users.noreply.github.com>
2024-12-13 02:43:44 +0100

    Add click events

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,22 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('Hi Larry', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        return (  
+            console.log('Hello ' + name, e.target)
+        );
+    }
+     
+
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('Laurens', e)}>Click me</button>
+
         </div>
      );
 }
