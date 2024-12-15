4ee4910ebbe01f8557ada8bf362374827a225616
LZYLRRY <162039530+LZYLRRY@users.noreply.github.com>
2024-12-15 01:57:29 +0100

    Add useState to Home component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,24 +1,36 @@
-const Home = () => {
+import { useState } from 'react';
 
-    const handleClick = (e) => {
-        console.log('Hi Larry', e);
+const Home = () => {
+    // let name = 'Mario'
+    const [name, setName] = useState('mario')
+    const [age, setAge] = useState(25)
+
+    const handleClick = () => {
+        if (name === 'mario') {
+            setName('luigi');
+            setAge(30);
+        } else {
+            setName('mario');
+            setAge(25);
+        }   
+        // setName('luigi');
     }
 
     const handleClickAgain = (name, e) => {
-        return (  
+        return (
             console.log('Hello ' + name, e.target)
         );
     }
-     
 
-    return ( 
+    return (
         <div className="home">
             <h2>Home Page</h2>
+            <p>{name} is {age} years old!</p>
             <button onClick={handleClick}>Click me</button>
             <button onClick={(e) => handleClickAgain('Laurens', e)}>Click me</button>
 
         </div>
-     );
+    );
 }
- 
+
 export default Home;
